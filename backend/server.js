091181925006b39ae91569e0ae598893c6a5cf2b
Ant0nioGl <ag53757@fer.hr>
2024-08-21 12:19:52 +0200

    Add support for CoinMarketCap API

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import cors from "cors";
+import env from "dotenv";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+env.config();
 
 /*
 const db = new pg.Client({
@@ -21,9 +24,23 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get("/api", (req, res) => {
-    return res.json({message: "This is from backend."});
-})
+
+app.get("/api/crypto-map", async (req, res) => {
+  try {
+    const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.API_KEY,
+      },
+    });
+    // Success
+    res.json(response.data);
+    
+  } catch (ex) {
+    // Fail
+    console.log(ex);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
 
 
 app.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,43 @@
 import { useState, useEffect } from 'react'
+import axios from "axios"
 import Navbar from './components/Navbar/Navbar.jsx'
 
 
 function App() {  
-  const [data, setData] = useState(null)
+  const [cryptoData, setCryptoData] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api")
-    .then(res => res.json())
-    .then(data => setData(data))
-    .catch(err => console.log("Error fetching data:", err));
-  }, [])
+    // Fetch data from the backend
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/crypto-map');
+        setCryptoData(response.data.data.filter(crypto => crypto.rank <= 50).slice(0, 20));
+      } catch (err) {
+        console.error('Error fetching data:', err);
+        setError('Failed to fetch cryptocurrency data');
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
+    <div>
       <Navbar />
-      <a> Message: {data === null ? "" : data.message}</a>
-    </>
-  )
+      {error && <p>{error}</p>}
+      <ul className='card-container'>
+        {cryptoData.map(crypto => (
+          <li className='card' key={crypto.id}>
+            <h3>{crypto.name} ({crypto.symbol})</h3>
+            <p>Rank: {crypto.rank}</p>
+            <p>Active: {crypto.is_active ? 'Yes' : 'No'}</p>
+            <p>First Historical Data: {new Date(crypto.first_historical_data).toLocaleDateString()}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App
