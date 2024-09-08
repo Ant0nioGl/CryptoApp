f4216906dca7f443f285e1b46798752b28e33d52
Ant0nioGl <ag53757@fer.hr>
2024-09-08 12:10:51 +0200

    Updates in banner style

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -4,7 +4,8 @@ function Banner() {
     return (
         <div className="banner-container">
             <div>
-                <h1 data-text="Explore crypto...">Explore crypto...</h1>
+                <h1 data-text="Explore crypto...">Explore <span className='color-text'>crypto</span>...</h1>
+                <h2>Start investing today.</h2>
             </div>
         </div>
     );
